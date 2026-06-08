7cc9d6e0d3bfdc7682364c551dbc9f2741b00100
Guilherme Nunes <guilherme.augd@gmail.com>
2026-06-08 16:06:04 -0300

    chore: set up custom logging injection

--- Fiap.Web.Students/Program.cs
+++ Fiap.Web.Students/Program.cs
@@ -1,4 +1,5 @@
 using Fiap.Web.Students.Data;
+using Fiap.Web.Students.Logging;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -14,6 +15,11 @@ if (string.IsNullOrWhiteSpace(connectionString))
 builder.Services.AddDbContext<DatabaseContext>(
     opt => opt.UseOracle(connectionString).EnableSensitiveDataLogging(builder.Environment.IsDevelopment())
 );
+
+#region Register IServiceCollection
+
+builder.Services.AddSingleton<ICustomLogger, MockLogger>();
+#endregion
 
 builder.Services.AddControllersWithViews();
 
